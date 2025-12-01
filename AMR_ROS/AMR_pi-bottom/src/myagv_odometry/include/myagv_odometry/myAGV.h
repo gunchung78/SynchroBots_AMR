1cc9ac0c23c8c31f57cdfacd16e76c895e302924
jyeop1224 <godjy1224@naver.com>
2025-12-01 16:01:37 +0900

    odom + imu

--- AMR_ROS/AMR_pi-bottom/src/myagv_odometry/include/myagv_odometry/myAGV.h
+++ AMR_ROS/AMR_pi-bottom/src/myagv_odometry/include/myagv_odometry/myAGV.h
@@ -1,84 +1,3 @@
-//#ifndef MYAGV_H
-//#define MYAGV_H
-//
-//#include <ros/ros.h>
-//#include <ros/time.h>
-//#include <geometry_msgs/Twist.h>
-//#include <nav_msgs/Odometry.h>
-//#include <geometry_msgs/TransformStamped.h>
-//#include <tf/transform_broadcaster.h>
-//#include <boost/asio.hpp>
-//#include <sensor_msgs/Imu.h>
-//
-//
-////#define sampleFreq	20.5f				// sample frequency in Hz
-//#define twoKpDef	1.0f				// (2.0f * 0.5f)	// 2 * proportional gain
-//#define twoKiDef	0.0f				// (2.0f * 0.0f)	// 2 * integral gain
-//#define OFFSET_COUNT 	200
-//
-//
-//#define OFFSET_COUNT 	200
-//
-//class MyAGV
-//{
-//public:
-//	MyAGV();
-//	~MyAGV();
-//	bool init();
-//	float invSqrt(float number);
-////	void execute(double linearX, double linearY, double angularZ);
-//	bool execute(double linearX, double linearY, double angularZ);
-//	void MahonyAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az);
-//	void accelerometerOffset(float gx, float gy, float gz);
-//	void publisherOdom();
-//	void publisherImuSensor();
-//	void publisherImuSensorRaw();
-//  void imuCallback(const sensor_msgs::Imu::ConstPtr& msg);
-//
-//private:
-//	bool readSpeed();
-//	void writeSpeed(double movex, double movey, double rot);
-//	void restore();
-//	void restoreRun();
-//
-//	ros::Time currentTime, lastTime;
-//
-//	double x;
-//	double y;
-//	double theta;
-//
-//	double vx;
-//	double vy;
-//	double vtheta;
-//
-//	double ax;
-//	double ay;
-//	double az;
-//
-//	double wx;
-//	double wy;
-//	double wz;
-//    
-////  double imu_roll, imu_pitch, imu_yaw; 
-////  bool imu_received = false;
-//  
-//	float Gyroscope_Xdata_Offset;
-//	float Gyroscope_Ydata_Offset;
-//	float Gyroscope_Zdata_Offset;
-//	float sampleFreq;
-//	unsigned short Offest_Count;
-//    sensor_msgs::Imu imu_data;
-//	ros::NodeHandle n;
-//  ros::Subscriber imu_sub;
-//	ros::Publisher pub_odom,pub_v,pub_imu,pub,pub_imu_raw;
-//	tf::TransformBroadcaster odomBroadcaster;
-//};
-//
-//extern bool imu_received;
-//extern double imu_yaw;
-//
-//#endif // !MYAGV_H
-
 #ifndef MYAGV_H
 #define MYAGV_H
 
@@ -108,7 +27,8 @@ public:
 	void execute(double linearX, double linearY, double angularZ);
     void MahonyAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az);
 	void accelerometerOffset(float gx, float gy, float gz);
-	void publisherOdom();
+	void publisherInternalOdom();
+  void publisherExternalOdom();
 	void publisherImuSensor();
 	void publisherImuSensorRaw();
 
@@ -147,7 +67,7 @@ private:
 	unsigned short Offest_Count;
     sensor_msgs::Imu imu_data;
 	ros::NodeHandle n;
-	ros::Publisher pub_odom,pub_v,pub_imu,pub,pub_imu_raw;
+	ros::Publisher pub_internal_imu_odom,pub_v,pub_imu,pub,pub_imu_raw, pub_external_imu_odom;
 	tf::TransformBroadcaster odomBroadcaster;
 };
 
